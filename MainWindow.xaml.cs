d23624e9bf61b658f1308564d929d11a7e803a25
ngoquangvinh <40285148+ngoquangvinh@users.noreply.github.com>
2025-03-26 22:13:45 +0700

    Changes date 26/03

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Lynx
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
