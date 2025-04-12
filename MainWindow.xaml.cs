62467a7bd1f36524538a36fb541575dabeb52587
NpqTrungzzzzz <quangtrung2002zzzzz@gmail.com>
2025-04-12 20:33:47 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+
+namespace Login
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer introTimer;
+        private int currentIndex = 0;
+
+        private readonly string[] introImages = new[]
+        {
+            "img/login.png",
+            "img/secure.png",
+            "img/fen.png",
+            "img/smart.png"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            introTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3)
+            };
+            introTimer.Tick += IntroTimer_Tick;
+            introTimer.Start();
+
+            ShowImage(currentIndex);
+        }
+
+        private void IntroTimer_Tick(object sender, EventArgs e)
+        {
+            var fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(400));
+            fadeOut.Completed += (s, _) =>
+            {
+                currentIndex = (currentIndex + 1) % introImages.Length;
+                ShowImage(currentIndex);
+
+                var fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(400));
+                IntroImage.BeginAnimation(OpacityProperty, fadeIn);
+            };
+
+            IntroImage.BeginAnimation(OpacityProperty, fadeOut);
+        }
+
+        private void ShowImage(int index)
+        {
+            try
+            {
+                string path = $"pack://application:,,,/{introImages[index]}";
+                IntroImage.Source = new BitmapImage(new Uri(path, UriKind.Absolute));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không load được ảnh: " + ex.Message);
+            }
+        }
+        private void SignUp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var registerWindow = new RegisterWindow(); 
+            registerWindow.Show();
+        }
+        private void ForgotPassword_Click(object sender, MouseButtonEventArgs e)
+        {
+            new ForgotPasswordWindow().Show();
+        }
+        private bool isPasswordVisible = false;
+
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible;
+
+            if (isPasswordVisible)
+            {
+                VisiblePasswordBox.Text = PasswordBox.Password;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                VisiblePasswordBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PasswordBox.Password = VisiblePasswordBox.Text;
+                PasswordBox.Visibility = Visibility.Visible;
+                VisiblePasswordBox.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ RegisterWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Login
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string fullname = FullNameBox.Text;
+            string username = UsernameBox.Text;
+            string phone = PhoneBox.Text;
+            string email = EmailBox.Text;
+            string password = PasswordBox.Password;
+            string confirmPassword = ConfirmPasswordBox.Password;
+            DateTime? birthday = BirthdayPicker.SelectedDate;
+
+            if (string.IsNullOrWhiteSpace(fullname) || string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(phone) || string.IsNullOrWhiteSpace(email) ||
+                string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(confirmPassword) ||
+                birthday == null)
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            MessageBox.Show("Registration successful!");
+            this.Close();
+        }
+    }
+}
