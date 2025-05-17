4af5f62bd205a89a879bb796b3e5d480f35e2874
ngoquangvinh <40285148+ngoquangvinh@users.noreply.github.com>
2025-05-17 18:41:52 +0700

    Add Sideprofile

--- Custom_Controls/Conversation.xaml.cs
+++ Custom_Controls/Conversation.xaml.cs
@@ -16,6 +16,24 @@ namespace LynxUI_Main.Custom_Controls
             DataContext = new ConversationViewModel();
         }
 
+        public static readonly DependencyProperty SelectedChatProperty =
+        DependencyProperty.Register("SelectedChat", typeof(ChatListItem), typeof(Conversation), new PropertyMetadata(null));
+
+        public ChatListItem SelectedChat
+        {
+            get => (ChatListItem)GetValue(SelectedChatProperty);
+            set => SetValue(SelectedChatProperty, value);
+        }
+
+        private static void OnSelectedChatChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as Conversation;
+            if (control?.DataContext is ConversationViewModel vm && e.NewValue is ChatListItem chat)
+            {
+                vm.ActiveChat = chat;
+            }
+        }
+
         private void BtnToggleSidebar_Click(object sender, RoutedEventArgs e)
         {
             if (Window.GetWindow(this) is MainWindow mainWindow)
---
+++ Custom_Controls/SideProfile.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LynxUI_Main.Custom_Controls
+{
+    /// <summary>
+    /// Interaction logic for SideProfile.xaml
+    /// </summary>
+    public partial class SideProfile : UserControl
+    {
+        public SideProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
